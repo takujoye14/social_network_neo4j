0d247cdb36258f0f85e16496d0548aedf626f66b
takujoye14 <takuma.joye414@gmail.com>
2025-04-29 18:58:11 +0200

    fixed code for adding a post and following

--- app.py
+++ app.py
@@ -238,11 +238,10 @@ def user_feed(user_id):
 
 @app.route('/create_post', methods=['POST'])
 def create_post():
-    user_id = int(request.form['user_id'])
+    user_id = request.form['user_id']  
     content = request.form['content']
     db.create_post(user_id, content)
     return redirect(url_for('user_profile', user_id=user_id))
-
 @app.route('/login/<user_id>')
 def login(user_id):
     session['user_id'] = user_id
@@ -255,8 +254,8 @@ def logout():
 
 @app.route('/follow', methods=['POST'])
 def follow():
-    follower_id = int(request.form['follower_id'])
-    followee_id = int(request.form['followee_id'])
+    follower_id = request.form['follower_id']
+    followee_id = request.form['followee_id']
     
     # Check if the user is already following
     following = db.get_following(follower_id)
